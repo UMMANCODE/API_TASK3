24a73d6538573a908b3ecb307d49716d8c55eecf
UMMANCODE <ummanmm@code.edu.az>
2024-06-06 18:11:10 +0400

    files added

---
+++ TASK3_Business/Exceptions/DublicateEntityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK3_Business.Exceptions {
+  public class DublicateEntityException : Exception {
+    public DublicateEntityException(string msg) : base(msg) { }
+    public DublicateEntityException() { }
+  }
+}
---
+++ TASK3_Business/Exceptions/EntityNotFound.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK3_Business.Exceptions {
+  public class EntityNotFoundException : Exception {
+    public EntityNotFoundException() { }
+  }
+}
